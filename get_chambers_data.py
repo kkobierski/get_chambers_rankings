bd67a6e5c243ed042b61d055e9300457c49bf01f
MJ Krac <github@c40.pl>
2019-04-25 10:38:27 +0200

    some fixes

--- get_chambers_data.py
+++ get_chambers_data.py
@@ -1,21 +1,24 @@
 import csv
-
 import requests
 
 URL_T2 = 'https://api.chambers.com/api/organisations/250204/ranked-lawyers?publicationTypeId=2'
 URL_T7 = 'https://api.chambers.com/api/organisations/111/ranked-lawyers?publicationTypeId=7'
 
 
-
 def translate_chambers_json(data):
     _cur_desc = data["description"]
-
-for i in data:
-    row_array = []
-    
-    row_array.append(data["description"])
-
-
+    for group in data["groups"]:
+        _cur_type = group["type"]
+        for practice in group["practiceAreas"]:
+            _cur_practice_id = practice["id"]
+            _cur_practice_desc = practice["description"]
+            for location in practice["individualsInLocations"]:
+                _cur_loc_id = location["id"]
+                _cur_loc_desc = location["description"]
+                for person in location["rankedEntities"]:
+                    _cur_person_id = person["personOrganisationId"]
+                    _cur_person_name = person["displayName"]
+                    for ranking in person["rankings"]:
                         _cur_rank_desc = ranking["rankingDescription"]
 
                     yield [
