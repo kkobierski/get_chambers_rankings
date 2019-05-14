e8c5f3fe3d3496dfc4ee0dd17e123bbde9c15a32
Ombre <ombre@ombre-mbp.local>
2019-05-14 11:47:46 +0200

    get and save both firm and individual rankings

--- get_chambers_data.py
+++ get_chambers_data.py
@@ -1,44 +1,14 @@
 import csv
 import requests
+from libs.translate_chambers_json import translate_chambers_json_individual
+from libs.translate_chambers_json import translate_chambers_json_firm
 
 firm_id = 111
-ranking_id = 2
+publication_id = 2
 
 
-url_firm = 'https://api.chambers.com/api/organisations/{}/ranked-departments?publicationTypeId={}'.format(firm_id,ranking_id)
-url_individual = 'https://api.chambers.com/api/organisations/{}/ranked-lawyers?publicationTypeId={}'.format(firm_id,ranking_id)
-
-
-
-def translate_chambers_json(data):
-    _cur_desc = data["description"]
-    _cur_firm_id = firm_id
-    for group in data["groups"]:
-        _cur_type = group["type"]
-        for practice in group["practiceAreas"]:
-            _cur_practice_id = practice["id"]
-            _cur_practice_desc = practice["description"]
-            for location in practice["individualsInLocations"]:
-                _cur_loc_id = location["id"]
-                _cur_loc_desc = location["description"]
-                for person in location["rankedEntities"]:
-                    _cur_person_id = person["personOrganisationId"]
-                    _cur_person_name = person["displayName"]
-                    for ranking in person["rankings"]:
-                        _cur_rank_desc = ranking["rankingDescription"]
-
-                    yield [
-                        _cur_desc,
-                        _cur_type,
-                        _cur_practice_id,
-                        _cur_practice_desc,
-                        _cur_loc_id,
-                        _cur_loc_desc,
-                        _cur_person_id,
-                        _cur_person_name,
-                        _cur_rank_desc,
-                        _cur_firm_id
-                    ]
+url_firm = 'https://api.chambers.com/api/organisations/{}/ranked-departments?publicationTypeId={}'.format(firm_id,publication_id)
+url_individual = 'https://api.chambers.com/api/organisations/{}/ranked-lawyers?publicationTypeId={}'.format(firm_id,publication_id)
 
 
 if __name__ == '__main__':
@@ -47,8 +17,22 @@ if __name__ == '__main__':
     except Exception:
         raise SystemExit('**Something** went wrong, debug it fool!')
 
-    with open('firm.csv', 'w') as file_obj:
+    with open('data/individuals_{}.csv'.format(firm_id), 'w') as file_obj:
         csv_writer = csv.writer(file_obj)
 
-        for row in translate_chambers_json(data):
+        for row in translate_chambers_json_individual(data):
             csv_writer.writerow(row)
+   
+    
+    
+    try:
+        data = requests.get(url_firm).json()
+    except Exception:
+        raise SystemExit('**Something** went wrong, debug it fool!')
+
+
+    with open('data/firm_{}.csv'.format(firm_id), 'w') as file_obj:
+            csv_writer = csv.writer(file_obj)
+    
+            for row in translate_chambers_json_firm(data):
+                csv_writer.writerow(row)
